f60dd490bc231e85a5d8f730f1569fd275e12d1b
RAGAV-24 <ragavrunner33@gmail.com>
2025-06-21 23:36:55 +0530

    added links

--- client/src/Components/Firebase.jsx
+++ client/src/Components/Firebase.jsx
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCfcy0KA3fK5rXfftPkk4HKSFqxH4u50l0",
+  authDomain:  "medical-chatbot-3ccc2.firebaseapp.com",
+  projectId: "medical-chatbot-3ccc2",
+  storageBucket:"medical-chatbot-3ccc2.firebasestorage.app",
+  messagingSenderId: "1083097145119",
+  appId:"1:1083097145119:web:5b19d7c0bd506bc5961a2a",
+  measurementId:  "G-S6MZLNY940",
 };
 
 const app = initializeApp(firebaseConfig);
